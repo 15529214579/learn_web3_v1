6ee0fd637ce2c2e5c382085aabe60160e68698a7
maxuefei <maxuefei@bigo.sg>
2023-07-23 23:06:21 +0800

    add 通过私钥生成地址

--- web3/go.mod
+++ web3/go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/btcsuite/btcutil v1.0.2 // indirect
 	github.com/bytedance/sonic v1.9.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
--- web3/wallet.go
+++ web3/wallet.go
@@ -4,7 +4,10 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
 	"log"
+
+	"github.com/btcsuite/btcutil/base58"
 )
 
 type Wallet struct {
@@ -24,4 +27,35 @@ func NewWallet() *Wallet {
 	pubKeyOrig := privateKey.PublicKey
 	pubKey := append(pubKeyOrig.X.Bytes(), pubKeyOrig.Y.Bytes()...)
 	return &Wallet{Private: privateKey, PubKey: pubKey}
+}
+
+func (w *Wallet) NewAddress() string {
+	pubkey := w.PubKey
+	hash := HashPubKey(pubkey)
+	version := byte(00)
+
+	payload := append([]byte{version}, hash...)
+
+	checkCode := CheckSum(payload)
+
+	payload = append(payload, checkCode...)
+
+	address := base58.Encode(payload)
+	return address
+}
+
+func HashPubKey(data []byte) []byte {
+	//此处代码copy,后续可替换成其他hash算法
+	hash := sha256.Sum256(data)
+
+	return hash[:]
+}
+
+func CheckSum(data []byte) []byte {
+	hash1 := sha256.Sum256(data)
+	hash2 := sha256.Sum256(hash1[:])
+
+	//返回前4位校验码
+	checkCode := hash2[:4]
+	return checkCode
 }
